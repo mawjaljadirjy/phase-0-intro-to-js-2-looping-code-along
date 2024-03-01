48b7b0046f452faae6559407006127ec954df7cd
Mawj_Aljadirjy <eng_mawj_aljadirjy@yahoo.com>
2024-03-01 22:01:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, event) {
+    const messages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+
+  function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
+  
+
+
